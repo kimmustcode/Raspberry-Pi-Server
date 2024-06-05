2353e667e5cf4c59c275a1b4de38fb862b0e663f
kimmustdie <sangwookim1999@gmail.com>
2024-06-05 13:31:32 -0700

    when new image opens kill prev process

--- For On Device/server.py
+++ For On Device/server.py
@@ -1,19 +1,51 @@
 from socket import * 
+import subprocess
+import sys
+import signal 
+from socket import *
+import os 
+import time 
 
 HOST = "0.0.0.0"
 PORT = 42069
+
+prev = ""
+liveProcs = [] 
+
+cmd = 'python myprograms/Raspberry-Pi-Server/For-Device/imageFrame.py https://www.kimcodes.lol/static/dakota4.png'
+p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, preexec_fns=os.setsid)
+
+c1 = p.pid 
+liveProcs.append(c1)
+c2 = p.pid + 1 
+liveProcs.append(c2)
 
 with socket(AF_INET, SOCK_STREAM) as s: 
     s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
 
-    
     while True:
         conn, addr = s.accept()
         data = conn.recv(1024)
-        if data: 
-            print(addr + ": " + str(data.decode('utf-8')))
-        if not data: 
-            break 
+        if data and data != prev: 
+            cmd = ['python myprograms/Raspberry-Pi-Server/For-Device/imageFrame.py' + str(data.decode('utf-8'))]
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setid)
+            c1 = p.pid 
+            c2 = p.pid +1 
+            time.sleep()
 
+            removed = []
+            for proc in liveProcs:
+                os.system("kill - 9" + str(proc))
+                removed.append(proc)
+
+            for proc in removed:
+                liveProcs.remove(proc)
+
+            removed = []
+
+            liveProcs.append(c1)
+            liveProcs.append(c2)
+
+            prev = data
